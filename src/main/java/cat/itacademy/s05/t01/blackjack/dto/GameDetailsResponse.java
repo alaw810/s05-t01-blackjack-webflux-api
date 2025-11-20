704f27802d0a80aaf194e9b203dbdcafba68bf7f
Adrià Lorente <adria810@gmail.com>
2025-11-20 11:10:49 +0100

    test(game): add service tests for getGame by id

---
+++ src/main/java/cat/itacademy/s05/t01/blackjack/dto/GameDetailsResponse.java
@@ -0,0 +1,18 @@
+package cat.itacademy.s05.t01.blackjack.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Builder
+public class GameDetailsResponse {
+    private String gameId;
+    private Long playerId;
+    private List<String> playerHand;
+    private List<String> dealerHand;
+    private String status;
+    private int playerHandValue;
+    private int dealerHandValue;
+    private int remainingDeckSize;
+}
