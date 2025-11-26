a22b6d8078afcf0d4778763b7a9c3bda033f798d
Adrià Lorente <adria810@gmail.com>
2025-11-26 13:17:29 +0100

    feat: add detailed GameStatus enum for explicit game end states

---
+++ src/main/java/cat/itacademy/s05/t01/blackjack/model/mongo/GameStatus.java
@@ -0,0 +1,10 @@
+package cat.itacademy.s05.t01.blackjack.model.mongo;
+
+public enum GameStatus {
+    IN_PROGRESS,
+    PLAYER_WIN,
+    PLAYER_LOSE,
+    PLAYER_BUST,
+    DEALER_BUST,
+    TIE
+}
